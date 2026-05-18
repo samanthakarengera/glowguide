a1e6c6d0e57a38ecf8cc7d47bd15e4f369d94789
samanthakarengera <karengerasamantha@gmail.com>
2026-05-18 15:22:08 +0200

    create welcomecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\WelcomeController::class,'index'])->name('welcome');
 
 Route::get('/dashboard', function () {
     return view('userzone.dashboard');
