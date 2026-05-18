299a4f6c5d33604cba27c673ee5c2fd19d060167
samanthakarengera <karengerasamantha@gmail.com>
2026-05-18 15:57:59 +0200

    create article

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Article>
+ */
+class ArticleFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' =>$this->faker->sentence(),
+            'content' =>$this->faker->paragraphs(5,true),
+            'published_at'=>$this->faker->dateTimeBetween('-1 month','now'),
+
+        ];
+    }
+}
